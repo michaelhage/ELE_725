162c93c64f21d11c2840ac61d4deed2c70dca0ac
michaelhage <56516464+michaelhage@users.noreply.github.com>
2020-01-29 01:04:56 -0500

    Update downsample.m

--- Labs/lab1/downsample.m
+++ Labs/lab1/downsample.m
@@ -15,33 +15,46 @@ function downsample(inFile, outFile, N, pf)
         aud_down(:,2) = decimate(aud(:,2), N);
     end
     
-    sound(aud,fs)
-    pause(5)
-    sound(aud_down, fs/N)
+    aud_interp(:,1) = interp(aud_down(:,1), N);
+    aud_interp(:,2) = interp(aud_down(:,2), N);
     
-    audiowrite(outFile, aud_down, fs/N);
-    
+%     Can reduce this to a single fft for each
     Aud1 = fft(aud(:,1));
     Aud2 = fft(aud(:,2));
     Aud_Down1 = fft(aud_down(:,1));
     Aud_Down2 = fft(aud_down(:,2));
+    Aud_Interp1 = fft(aud_interp(:,1));
+    Aud_Interp2 = fft(aud_interp(:,2));
     
-    plotx = {abs(Aud1), abs(Aud2), abs(Aud_Down1), abs(Aud_Down2)};
-    str = {'Original Audio(1)', 'Original Audio(2)', 'Downsampled Audio(1)', 'Downsampled Audio(2)'};
+    disp('Original Audio')
+    sound(aud,fs)
+    pause(5)
+    disp('Downsampled Audio')
+    sound(aud_down, fs/N)
+    pause(5)
+    disp('Interpolated Audio')
+    sound(aud_interp,fs)
+    
+    plotx = {abs(Aud1), abs(Aud2), abs(Aud_Down1), abs(Aud_Down2), ...
+        abs(Aud_Interp1), abs(Aud_Interp2)};
+    str = {'Original Audio(1)', 'Original Audio(2)', ...
+        'Downsampled Audio(1)','Downsampled Audio(2)', ...
+        'Reconstructed Audio(1)', 'Reconstructed Audio(2)'};
 
-    figure
+    
     for i = 1:length(plotx)
-        subplot(2,length(plotx),i);
+        figure
+        
+        subplot(1,2,1);
         spectrogram(fftshift(plotx{i}));
         title(str{i});
         xlabel('Frequency');
         
-        subplot(2,length(plotx),i+3)
-        plot(fftshift(plotx{i}));
+        subplot(1,2,2)
+        stem(fftshift(plotx{i}));
         title(str{i});
         xlabel('Frequency');
     end
     
-
+    audiowrite(outFile, aud_down, fs/N);
 end
-
